3bf51004223450142349f410ddfd04d4e9a6f86a
Sudeeeep <sudeepn15@gmail.com>
2023-02-15 20:04:37 +0530

    Exercises 2.16.-2.17.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,18 +3,20 @@ import Filter from "./components/Filter";
 import PersonsForm from "./components/PersonsForm";
 import Persons from "./components/Persons";
 import personsService from "./services/persons";
+import Notification from "./components/Notification";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newFilter, setNewFilter] = useState("");
+  const [notification, setNotification] = useState(null);
 
   useEffect(() => {
     personsService.getAllPersons().then((returnedPersons) => {
       setPersons(returnedPersons);
     });
-  });
+  }, []);
 
   const addNewContact = (e) => {
     e.preventDefault();
@@ -34,14 +36,24 @@ const App = () => {
         personsService
           .updatePerson(requiredPerson.id, newObj)
           .then((returnedPerson) => {
-            persons.splice(requiredPerson.id - 1, 1);
-            setPersons(persons.concat(returnedPerson));
+            persons.splice(requiredPerson.id - 1, 1, returnedPerson);
+            setPersons([...persons]);
+            setNotification(`Updated ${newName}`);
+            resetNotification();
+          })
+          .catch((err) => {
+            setNotification(
+              `Information of ${newName} has already been removed from the server`
+            );
+            resetNotification();
           });
       }
     } else {
-      personsService
-        .addPerson(newObj)
-        .then((returnedPersons) => setPersons(persons.concat(returnedPersons)));
+      personsService.addPerson(newObj).then((returnedPersons) => {
+        setPersons(persons.concat(returnedPersons));
+        setNotification(`Added ${newName}`);
+        resetNotification();
+      });
     }
 
     setNewName("");
@@ -50,13 +62,32 @@ const App = () => {
 
   const deletePerson = (id, name) => {
     if (window.confirm(`Do you really want to delete ${name} ?`)) {
-      personsService.deletePerson(id).then(setPersons([...persons]));
+      personsService
+        .deletePerson(id)
+        .then(() => {
+          persons.splice(id - 1, 1);
+          setPersons([...persons]);
+          setNotification(`Deleted ${name}`);
+          resetNotification();
+        })
+        .catch((err) => {
+          setNotification(
+            `Information of ${name} has already been removed from the server`
+          );
+          resetNotification();
+        });
     }
+  };
+
+  const resetNotification = () => {
+    setTimeout(() => setNotification(null), 5000);
   };
 
   return (
     <div>
       <h1>Phonebook</h1>
+
+      <Notification message={notification} />
 
       <Filter setNewFilter={setNewFilter} />
 
