1b5f3bfd0dbffc3121f12bc6b519e37891c1c96d
Sudeeeep <sudeepn15@gmail.com>
2023-03-28 16:50:13 +0530

    add Exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,40 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [moreDetails, setMoreDetails] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  if (!moreDetails) {
+    return (
+      <div style={blogStyle}>
+        <div>
+          {blog.title} {blog.author}
+          <button onClick={() => setMoreDetails(true)}>view</button>
+        </div>
+      </div>
+    );
+  }
+
+  if (moreDetails) {
+    return (
+      <div style={blogStyle}>
+        <div>
+          {blog.title} {blog.author}{" "}
+          <button onClick={() => setMoreDetails(false)}>hide</button> <br />
+          {blog.url} <br />
+          likes {blog.likes} <button>like</button> <br />
+          {blog.user.name}
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Blog;
